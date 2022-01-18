84a9b8e8ad53d71088db1ab40444d59c757da7bc
zook2per <97614199+zook2per@users.noreply.github.com>
2022-01-18 23:54:08 +0900

    studying variables

--- moment-app/app.js
+++ moment-app/app.js
@@ -1,1 +1,11 @@
-alert("HELLO THIS IS MOMENT APPLICATION!")
+const a = 5;
+const b = 2;
+let myName = "ZOO";
+
+console.log(a+b);
+console.log(a*b);
+console.log("Hi, I'm " + myName);
+
+myName = "JI WOO";
+
+console.log("BYE, My real name is " + myName);
