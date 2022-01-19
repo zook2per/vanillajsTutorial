2feba529e406e497cc04c2aafd45b4fd4abcaab1
zook2per <97614199+zook2per@users.noreply.github.com>
2022-01-19 23:54:53 +0900

    changing css style

--- moment-app/app.js
+++ moment-app/app.js
@@ -1,11 +1,1 @@
-const a = 5;
-const b = 2;
-let myName = "ZOO";
-
-console.log(a+b);
-console.log(a*b);
-console.log("Hi, I'm " + myName);
-
-myName = "JI WOO";
-
-console.log("BYE, My real name is " + myName);
+console.log ("we got it");
